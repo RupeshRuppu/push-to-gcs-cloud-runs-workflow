e5588c743b6141fdc13dbd1abcbc03ebf842b1f1
Rupesh Lakshman <codewithruppu@gmail.com>
2026-02-22 23:34:03 +0530

    zipped bucket name change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			output: {
+				entryFileNames: "apps/[name].js",
+				chunkFileNames: "apps/[name].js",
+				assetFileNames: assetInfo => {
+					const name = assetInfo.name ?? "";
+					const ext = name.split(".").pop()?.toLowerCase();
+					if (ext === "css") {
+						return "apps/[name][extname]";
+					}
+					return "fonts/[name][extname]";
+				},
+			},
+		},
+	},
+});
