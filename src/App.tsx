f118a9f7a242dbd188a891a582e6cabdc00c3f25
Rupesh Lakshman <93902770+RupeshRuppu@users.noreply.github.com>
2026-07-02 23:51:09 +0530

    Remove unnecessary comment from App component
    Removed an unnecessary comment from the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
-/* Added a comment */
 function App() {
 	const [count, setCount] = useState(0);
 
