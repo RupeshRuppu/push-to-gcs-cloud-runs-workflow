44caa38c8d751094286b7e9f296758304eab2db2
Rupesh Lakshman <93902770+RupeshRuppu@users.noreply.github.com>
2026-07-02 23:24:51 +0530

    Add comment in App component
    Added a comment to the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
 
+/* Added a comment */
 function App() {
 	const [count, setCount] = useState(0);
 
